31c4118a6c00a555a5c352e61a878d8c2f9bf4e6
tom <tom@wembli.com>
2012-05-19 01:06:08 -0700

    added findplanbyfriend

--- models/customer.js
+++ models/customer.js
@@ -1,3 +1,6 @@
+var wembliUtils   = require('wembli/utils');
+var async         = require('async');
+
 this.Model = function(mongoose) {
     var Schema = mongoose.Schema;
     var ObjectId = Schema.ObjectId;
@@ -59,6 +62,7 @@ this.Model = function(mongoose) {
 
     Customer.pre('save',function(next) {
 	this.last_modified = new Date();
+	console.log('pre save');
 	next();
     });
 
@@ -100,6 +104,94 @@ this.Model = function(mongoose) {
 		callback(err);
 	    }
 	});
+    };
+
+    Customer.statics.findPlansByFriend = function(friend,callback) {
+	var Customer = this;
+	var friends = [];
+	var tasks = [];
+	if ((typeof friend.fbId != "undefined") && (friend.fbId != null)) {
+	    var getByFb = function(cb1) {
+		//if there's a fbId for this customer get all the friends that have this as an id
+		var query = Customer.find();
+		query.where('eventplan.friends').elemMatch(function(elem) {
+		    elem.where('fbId',friend.fbId);
+		});
+		query.exec(function(err,res) {
+		    if (err) {
+			cb1(err);
+		    } else {
+			var plans = [];
+			for (var i in res) {
+			    //get the eventplans where this fbId is a friend
+			    for (var idx in res[i].eventplan) {
+				var plan = res[i].eventplan[idx];
+				var keep = false;
+				for (var idx2 in plan.friends) {
+				    var f = plan.friends[idx2];
+				    if ((typeof f.fbId != "undefined") && (f.fbId == friend.fbId)) {
+					plan.friends[idx2].me = true;
+					plans.push(plan);
+					break;
+				    }
+				}
+			    }
+			}
+			cb1(null,plans);
+		    }
+		});
+	    };
+	    tasks.push(getByFb);
+	}
+
+	if ((typeof friend.email != "undefined") && (friend.email != null)) {
+	    var getByEmail = function(cb2) {
+		//if there's a fbId for this customer get all the friends that have this as an id
+		var query = Customer.find();
+		query.where('eventplan.friends').elemMatch(function(elem) {
+		    elem.where('email',friend.email);
+		});
+		query.exec(function(err,res) {
+		    if (err) {
+			cb2(err);
+		    } else {
+			var plans = [];
+			//get the eventplans where this fbId is a friend
+			for (var i in res) {
+			    for (var idx in res[i].eventplan) {
+				var plan = res[i].eventplan[idx];
+				var keep = false;
+				for (var idx2 in plan.friends) {
+				    var f = plan.friends[idx2];
+				    if ((typeof f.email != "undefined") && (f.email == friend.email)) {
+					plan.friends[idx2].me = true;
+					plans.push(plan);
+					break;
+				    }
+				}
+			    }
+			}
+			cb2(null,plans);
+		    }
+		});
+	    };
+	    tasks.push(getByEmail);
+	}
+
+	//merge the results
+	var attending = [];
+
+	if (tasks.length > 0) {
+	    async.parallel(tasks,function(err,results) {
+		for (var idx in results) {
+		    wembliUtils.merge(attending,results[idx]);
+		}
+		callback(null,attending);
+	    });
+	} else {
+	    callback(null,attending);
+	}
+	
     };
 
     Customer.statics.findPlanByGuid = function(guid,callback) {
