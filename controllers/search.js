17bf3467a65fcf15e1802233b1c820a39eba5eca
Tom Walpole <tom@tomwalpole.com>
2013-01-16 21:53:41 -0800

    moved search and added index

--- controllers/search.js
+++ controllers/search.js
@@ -1,10 +1,49 @@
-var querystring = require('querystring');
+var ticketNetwork = require('../lib/wembli/ticketnetwork');
+var async         = require('async');
 var eventRpc      = require('../rpc/event').event;
+var dateUtils     = require('date-utils');
+var ua            = require('useragent');
+//ua(true); //sync with remote server to make sure we have the latest and greatest
+var querystring = require('querystring');
 
 module.exports = function(app) {
 
+
+
+	app.get('/search/?', function(req, res) {
+
+		//clear the updateEvent session so searches start over
+		//this session variable allows you to swap out the event for an existing plan
+		delete req.session.updateEvent;
+
+		/*
+			args holds the args for the TN GetEvent call
+			args.beginDate
+			args.orderByClause
+			args.nearZip
+		*/
+		var args = {};
+		args.beginDate = getBeginDate();
+		args.orderByClause = 'Date'; //order by date
+		if (typeof req.session.ipinfo != "undefined") {
+			args.nearZip = req.session.ipinfo.postal_code;
+		}
+
+		//get nearby events:
+		eventRpc['get'].apply(function(err,results) {
+			console.log('results from eventrpc: ');
+			console.log(results);
+			res.render('search', {
+				events: results.event,
+				title: 'wembli.com - Tickets, Parking, Restaurant Deals - All Here.',
+			});
+		},[args,req,res]);
+
+	});
+
+
 	//these come from the more events button
-	app.get('/search/:city/:from', function(req,res) {
+	app.get('/search/events/:city/:from', function(req,res) {
 
 		var whereClause = '';
 		if (req.param('from')) {
@@ -21,7 +60,7 @@ module.exports = function(app) {
 			console.log('results from eventrpc: ');
 			console.log(results);
 
-			res.render('index', {
+			res.render('search', {
 				search: req.param('city'),
 				events: results.event,
 				title: 'wembli.com - Tickets, Parking, Restaurant Deals - All Here.',
@@ -30,7 +69,7 @@ module.exports = function(app) {
 	});
 
 
-	app.get('/search/:query?', function(req, res) {
+	app.get('/search/events/:query?', function(req, res) {
 		console.log(req.param('search'));
 
 		var title = 'Wembli Search';
@@ -38,7 +77,7 @@ module.exports = function(app) {
 		var query = req.param('query') ? querystring.unescape(req.param('query')).replace(/\+/g,' ') : querystring.unescape(req.param('search')).replace(/\+/g,' ');
 		console.log('query: '+query);
 		if (!query) {
-			return res.redirect('/');
+			return res.redirect('/search');
 		}
 
 		query.replace('+',' ');
@@ -55,11 +94,23 @@ module.exports = function(app) {
 			console.log('results from eventrpc: ');
 			console.log(results);
 
-			res.render('index', {
+			res.render('search', {
 				search: query,
 				events: results.event,
 				title: 'wembli.com - Tickets, Parking, Restaurant Deals - All Here.',
 			});
 		},[args,req,res]);
 	});
+
+};
+
+//not sure what the best begin date is for the events list on the home page
+//for now we will get events that begin 2 days from today so we have minimal fulfillment issues
+function getBeginDate() {
+	var daysPadding = 2; //how many days from today for the beginDate
+	var d = Date.today();
+	d2 = new Date(d);
+	d2.setDate(d.getDate() + 2);
+	//format the beginDate for the tn query
+	return d2.format("shortDate");
 }
