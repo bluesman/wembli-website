4747f4ecbd18a7b4c3ca207a86794373b254459d
Tom Walpole <tom@tomwalpole.com>
2013-04-08 02:32:58 -0700

    more dashboard

--- bin/uglify-wembli.sh
+++ bin/uglify-wembli.sh
@@ -1,4 +1,4 @@
 cd /wembli/website/public;
-uglifyjs js/plugins/sequence.jquery-min.js js/lib/angular/angular.min.js js/app.js js/controllers.js js/services.js js/directives.js js/filters.js js/twitter-bootstrap/bootstrap-alert.js js/twitter-bootstrap/bootstrap-transition.js js/twitter-bootstrap/bootstrap-tooltip.js js/twitter-bootstrap/bootstrap-popover.js js/plugins/pikaday.jquery.js js/bootstrap-modal/bootstrap-modalmanager.js js/bootstrap-modal/bootstrap-modal.js -o js/wembli.min.js 
+uglifyjs js/plugins/sequence.jquery-min.js js/lib/angular/angular.min.js js/app.js js/controllers.js js/services.js js/directives.js js/filters.js js/twitter-bootstrap/bootstrap-alert.js js/twitter-bootstrap/bootstrap-transition.js js/twitter-bootstrap/bootstrap-tooltip.js js/twitter-bootstrap/bootstrap-dropdown.js js/twitter-bootstrap/bootstrap-popover.js js/plugins/pikaday.jquery.js js/bootstrap-modal/bootstrap-modalmanager.js js/bootstrap-modal/bootstrap-modal.js -o js/wembli.min.js 
 echo "uglified...
 ";
